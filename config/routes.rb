e8317ad93c6b02830b362c9d04a1173a0648e2dc
Janaya Gripper <jnygripper@gmail.com>
2022-05-06 08:30:07 -0400

    added schema, + content and styling to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'questions', to: 'pages#questions'
+  get '/', to: 'pages#home'
   resources :guests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
